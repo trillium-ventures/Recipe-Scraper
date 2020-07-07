fd3f22949a27d573a7fc7f0ecd270024165d5dc9
Daena Ramiah <daenaramiah@erichs-imac-pro.lan>
2020-07-07 12:08:58 -0700

    Added taste cooking scraper.

---
+++ scrapers/tastecooking.js
@@ -0,0 +1,60 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+const RecipeSchema = require("recipe-scraper/helpers/recipe-schema");
+
+const TasteCooking = url => {
+  const Recipe = new RecipeSchema();
+  return new Promise((resolve, reject) => {
+    if (!url.includes("tastecooking.com/")) {
+      reject(new Error("url provided must include 'tastecooking.com/'"));
+    } else {
+      request(url, (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+          const $ = cheerio.load(html);
+
+          Recipe.image = $("meta[name='twitter:image']").attr("content");
+          Recipe.name = $("meta[property='og:title']").attr("content");
+
+
+          $(".recipe-body-ingredient").each((i, el) => {
+            var ingredientString = ""
+            var quantity = $(el).find(".recipe-body-ingredient-quantity").find(".ingredient-number").text().trim();
+            var quant_label = $(el).find(".recipe-body-ingredient-quantity").find(".ingredient-label").text().trim();
+            if (quant_label === "c") {
+              quant_label = "cups"
+            }
+            var ingredient = $(el).find(".recipe-body-ingredient-name").text().trim();
+            ingredientString = ingredientString.concat(quantity, " ", quant_label, " ", ingredient);
+            Recipe.ingredients.push(ingredientString);
+          })
+
+          Recipe.servings = $('[itemprop="recipeYield"]')
+            .children(".recipe-stats-quantity")
+            .text()
+            .trim();
+
+          $(".recipe-body-list-container").find("li").each((i, el) => {
+            console.log("HERE IS EL TEXT: ", $(el).text());
+            Recipe.instructions.push($(el).text().trim());
+          })
+
+          Recipe.time = "";
+
+          if (
+            !Recipe.name ||
+            !Recipe.ingredients.length
+          ) {
+            reject(new Error("No recipe found on page"));
+          } else {
+            resolve(Recipe);
+          }
+        } else {
+          reject(new Error("No recipe found on page"));
+        }
+      });
+    }
+  });
+};
+
+module.exports = TasteCooking;
